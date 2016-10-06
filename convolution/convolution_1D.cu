f5233dd63b73284d8ee712a4d4b818f375015189
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-10-06 12:26:44 -0500

    convolution 2D in GPU

--- convolution/convolution_1D.cu
+++ convolution/convolution_1D.cu
@@ -10,104 +10,104 @@ using namespace std;
 
 // Secuencial
 void convolution_1D(double *v, double *mask, double *result) {
-	for (int i = 0; i < WIDTH; i++) {
-		double Pvalue = 0;
-		int N_start_point = i - (MASK_WIDTH / 2);
-		for (int j = 0; j < MASK_WIDTH; j++) {
-			if (N_start_point + j >= 0 && N_start_point + j < WIDTH) {
-				Pvalue += v[N_start_point + j] * mask[j];
-			}
-		}
-		result[i] = Pvalue;
-	}
+  for (int i = 0; i < WIDTH; i++) {
+    double Pvalue = 0;
+    int N_start_point = i - (MASK_WIDTH / 2);
+    for (int j = 0; j < MASK_WIDTH; j++) {
+      if (N_start_point + j >= 0 && N_start_point + j < WIDTH) {
+        Pvalue += v[N_start_point + j] * mask[j];
+      }
+    }
+    result[i] = Pvalue;
+  }
 }
 
 __global__
 void convolution_1D_kernel(double *v, double *mask, double *result) {
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	double Pvalue = 0;
-	int N_start_point = idx - (MASK_WIDTH / 2);
-	for (int j = 0; j < MASK_WIDTH; j++) {
-		if (N_start_point + j >= 0 && N_start_point + j < WIDTH) {
-			Pvalue += v[N_start_point + j] * mask[j];
-		}
-	}
-	result[idx] = Pvalue;
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  double Pvalue = 0;
+  int N_start_point = idx - (MASK_WIDTH / 2);
+  for (int j = 0; j < MASK_WIDTH; j++) {
+    if (N_start_point + j >= 0 && N_start_point + j < WIDTH) {
+      Pvalue += v[N_start_point + j] * mask[j];
+    }
+  }
+  result[idx] = Pvalue;
 }
 
 
 void printVector(double *v) {
-	cout << "[";
-	for (int i = 0; i < WIDTH; i++) {
-		if (i) cout << ", ";
-		cout << setprecision(0) << v[i];
-	}
-	cout << "]" << endl;
+  cout << "[";
+  for (int i = 0; i < WIDTH; i++) {
+    if (i) cout << ", ";
+    cout << setprecision(0) << v[i];
+  }
+  cout << "]" << endl;
 }
 
 void fillVector(double *v) {
-	for (int i = 0; i < WIDTH; i++) {
-		v[i] = i + 1;
-	}
+  for (int i = 0; i < WIDTH; i++) {
+    v[i] = i + 1;
+  }
 }
 
 int main() {
-	// Host variables
-	double h_mask[] = {3, 4, 5, 4, 3};
-	double *h_v = new double[WIDTH];
-	double *h_result = new double[WIDTH];
-	double *ans = new double[WIDTH];
+  // Host variables
+  double h_mask[] = {3, 4, 5, 4, 3};
+  double *h_v = new double[WIDTH];
+  double *h_result = new double[WIDTH];
+  double *ans = new double[WIDTH];
 
-	cout << fixed;
-	fillVector(h_v);
+  cout << fixed;
+  fillVector(h_v);
 
-	// Device
-	{
-		clock_t start = clock();
+  // Host
+  {
+    clock_t start = clock();
 
-		convolution_1D(h_v, h_mask, h_result);
-		printVector(h_v);
-		printVector(h_result);
+    convolution_1D(h_v, h_mask, h_result);
+    printVector(h_v);
+    printVector(h_result);
 
-		clock_t end = clock();
-		double time_used = double(end - start) / CLOCKS_PER_SEC;
-		cout << "Tiempo invertido CPU = " << setprecision(10) << time_used << "s" << endl << endl;
-	}
+    clock_t end = clock();
+    double time_used = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Tiempo invertido CPU = " << setprecision(10) << time_used << "s" << endl << endl;
+  }
 
-	// Device variables
-	double *d_mask, *d_v, *d_result;
-	int blockSize = 4;
-	dim3 dimBlock(blockSize, 1, 1);
-	dim3 dimGrid(ceil(WIDTH / float(blockSize)), 1, 1);
+  // Device variables
+  double *d_mask, *d_v, *d_result;
+  int blockSize = 4;
+  dim3 dimBlock(blockSize, 1, 1);
+  dim3 dimGrid(ceil(WIDTH / float(blockSize)), 1, 1);
 
-	cudaMalloc(&d_mask, sizeof(double) * MASK_WIDTH);
-	cudaMalloc(&d_v, sizeof(double) * WIDTH);
-	cudaMalloc(&d_result, sizeof(double) * WIDTH);
+  cudaMalloc(&d_mask, sizeof(double) * MASK_WIDTH);
+  cudaMalloc(&d_v, sizeof(double) * WIDTH);
+  cudaMalloc(&d_result, sizeof(double) * WIDTH);
 
-	// Host
-	{
-		clock_t start = clock();
+  // Device
+  {
+    clock_t start = clock();
 
-		cudaMemcpy(d_v, h_v, sizeof(double) * WIDTH, cudaMemcpyHostToDevice);
-		cudaMemcpy(d_mask, h_mask, sizeof(double) * MASK_WIDTH, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_v, h_v, sizeof(double) * WIDTH, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mask, h_mask, sizeof(double) * MASK_WIDTH, cudaMemcpyHostToDevice);
 
-		convolution_1D_kernel<<< dimGrid, dimBlock >>>(d_v, d_mask, d_result);
-		cudaMemcpy(ans, d_result, sizeof(double) * WIDTH, cudaMemcpyDeviceToHost);
-		printVector(h_v);
-		printVector(ans);
+    convolution_1D_kernel<<< dimGrid, dimBlock >>>(d_v, d_mask, d_result);
+    cudaMemcpy(ans, d_result, sizeof(double) * WIDTH, cudaMemcpyDeviceToHost);
+    printVector(h_v);
+    printVector(ans);
 
-		clock_t end = clock();
-		double time_used = double(end - start) / CLOCKS_PER_SEC;
-		cout << "Tiempo invertido GPU = " << setprecision(10) << time_used << "s" << endl;
-	}
+    clock_t end = clock();
+    double time_used = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Tiempo invertido GPU = " << setprecision(10) << time_used << "s" << endl;
+  }
 
-	delete h_v;
-	delete h_result;
-	delete ans;
+  delete h_v;
+  delete h_result;
+  delete ans;
 
-	cudaFree(d_mask);
-	cudaFree(d_v);
-	cudaFree(d_result);
+  cudaFree(d_mask);
+  cudaFree(d_v);
+  cudaFree(d_result);
 
-	return 0;
+  return 0;
 }
--- convolution/convolution_2D.cu
+++ convolution/convolution_2D.cu
@@ -9,68 +9,128 @@ using namespace std;
 #define WIDTH 7
 
 // Secuencial
-void convolution_2D(double *v, double *mask, double *result) {
-	for (int i = 0; i < WIDTH; i++) {
-		for (int j = 0; j < WIDTH; j++) {
-			double Pvalue = 0;
+void convolution_2D(double *m, double *mask, double *result) {
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      double Pvalue = 0;
 
-			int N_start_point = i - (MASK_WIDTH / 2);
-			int M_start_point = j - (MASK_WIDTH / 2);
+      int N_start_point = i - (MASK_WIDTH / 2);
+      int M_start_point = j - (MASK_WIDTH / 2);
 
-			for (int ii = 0; ii < MASK_WIDTH; ii++) {
-				for (int jj = 0; jj < MASK_WIDTH; jj++) {
-					if (N_start_point + ii >= 0 && N_start_point + ii < WIDTH &&
-							M_start_point + jj >= 0 && M_start_point + jj < WIDTH) {
-						Pvalue += v[WIDTH * (N_start_point + ii) + (M_start_point + jj)] * mask[MASK_WIDTH * ii + jj];
-					}
-				}
-			}
-			result[WIDTH * i + j] = Pvalue;
-		}
-	}
+      for (int ii = 0; ii < MASK_WIDTH; ii++) {
+        for (int jj = 0; jj < MASK_WIDTH; jj++) {
+          if (N_start_point + ii >= 0 && N_start_point + ii < WIDTH &&
+              M_start_point + jj >= 0 && M_start_point + jj < WIDTH) {
+            Pvalue += m[WIDTH * (N_start_point + ii) + (M_start_point + jj)] * mask[MASK_WIDTH * ii + jj];
+          }
+        }
+      }
+      result[WIDTH * i + j] = Pvalue;
+    }
+  }
+}
+
+__global__
+void convolution_2D_kernel(double *m, double *mask, double *result) {
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (row < WIDTH && col < WIDTH) {
+    double Pvalue = 0;
+
+    int N_start_point = row - (MASK_WIDTH / 2);
+    int M_start_point = col - (MASK_WIDTH / 2);
+
+    for (int i = 0; i < MASK_WIDTH; i++) {
+      for (int j = 0; j < MASK_WIDTH; j++) {
+        if (N_start_point + i >= 0 && N_start_point + i < WIDTH &&
+            M_start_point + j >= 0 && M_start_point + j < WIDTH) {
+          Pvalue += m[WIDTH * (N_start_point + i) + (M_start_point + j)] * mask[MASK_WIDTH * i + j];
+        }
+      }
+    }
+    result[WIDTH * row + col] = Pvalue;
+  }
 }
 
 void printMatrix(double *v) {
-	for (int i = 0; i < WIDTH; i++) {
-		cout << "[";
-		for (int j = 0; j < WIDTH; j++) {
-			if (j) cout << ", ";
-			cout << v[WIDTH * i + j];
-		}
-		cout << "]" << endl;
-	}
-	cout << endl;
+  for (int i = 0; i < WIDTH; i++) {
+    cout << "[";
+    for (int j = 0; j < WIDTH; j++) {
+      if (j) cout << ", ";
+      cout << v[WIDTH * i + j];
+    }
+    cout << "]" << endl;
+  }
+  cout << endl;
 }
 
 void fillMatrix(double *v) {
-	for (int i = 0; i < WIDTH; i++) {
-		for (int j = 0; j < WIDTH; j++) {
-			v[WIDTH * i + j] = j + 1;
-		}
-	}
+  for (int i = 0; i < WIDTH; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      v[WIDTH * i + j] = j + 1;
+    }
+  }
 }
 
 int main() {
-	double h_mask[] = {1, 2, 3, 2, 1, \
-										 2, 3, 4, 3, 2, \
-										 3, 4, 5, 4, 3, \
-										 2, 3, 4, 3, 2, \
-										 1, 2, 3, 2, 1};
+  // Host
+  double h_mask[] = {1, 2, 3, 2, 1, \
+                     2, 3, 4, 3, 2, \
+                     3, 4, 5, 4, 3, \
+                     2, 3, 4, 3, 2, \
+                     1, 2, 3, 2, 1};
 
-	double h_m[] = {1, 2, 3, 4, 5, 6, 7, \
-								 2, 3, 4, 5, 6, 7, 8, \
-								 3, 4, 5, 6, 7, 8, 9, \
-								 4, 5, 6, 7, 8, 5, 6, \
-								 5, 6, 7, 8, 5, 6, 7, \
-								 6, 7, 8, 9, 0, 1, 2, \
-								 7, 8, 9, 0, 1, 2, 3};
+  double h_m[] = {1, 2, 3, 4, 5, 6, 7, \
+                 2, 3, 4, 5, 6, 7, 8, \
+                 3, 4, 5, 6, 7, 8, 9, \
+                 4, 5, 6, 7, 8, 5, 6, \
+                 5, 6, 7, 8, 5, 6, 7, \
+                 6, 7, 8, 9, 0, 1, 2, \
+                 7, 8, 9, 0, 1, 2, 3};
 
-	double *h_result = new double[WIDTH * WIDTH];
+  double *h_result = new double[WIDTH * WIDTH];
+  double *ans = new double[WIDTH * WIDTH];
+  // fillMatrix(h_m);
 
-	// fillMatrix(h_m);
-	convolution_2D(h_m, h_mask, h_result);
-	printMatrix(h_m);
-	printMatrix(h_result);
+  {
+    clock_t start = clock();
 
-	return 0;
+    convolution_2D(h_m, h_mask, h_result);
+    printMatrix(h_m);
+    printMatrix(h_result);
+
+    clock_t end = clock();
+    double time_used = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Tiempo invertido CPU = " << setprecision(10) << time_used << "s" << endl << endl;
+  }
+
+  // Device
+  double *d_mask, *d_m, *d_result;
+  int blockSize = 4;
+  dim3 dimBlock(blockSize, blockSize, 1);
+  dim3 dimGrid(ceil(WIDTH / float(blockSize)), ceil(WIDTH / float(blockSize)), 1);
+
+  cudaMalloc(&d_mask, sizeof(double) * MASK_WIDTH * MASK_WIDTH);
+  cudaMalloc(&d_m, sizeof(double) * WIDTH * WIDTH);
+  cudaMalloc(&d_result, sizeof(double) * WIDTH * WIDTH);
+
+  // Device
+  {
+    clock_t start = clock();
+
+    cudaMemcpy(d_m, h_m, sizeof(double) * WIDTH * WIDTH, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mask, h_mask, sizeof(double) * MASK_WIDTH * MASK_WIDTH, cudaMemcpyHostToDevice);
+
+    convolution_2D_kernel<<< dimGrid, dimBlock >>>(d_m, d_mask, d_result);
+    cudaMemcpy(ans, d_result, sizeof(double) * WIDTH * WIDTH, cudaMemcpyDeviceToHost);
+    printMatrix(h_m);
+    printMatrix(ans);
+
+    clock_t end = clock();
+    double time_used = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Tiempo invertido GPU = " << setprecision(10) << time_used << "s" << endl << endl;
+  }
+
+  return 0;
 }
